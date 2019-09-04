c889dce0e03814364d4b3d375d7061aebeec7c52
mateusb12 <mateus_b09@hotmail.com>
2019-09-04 10:59:36 -0300

    Add files via upload

--- AV1/src/Execution.java
+++ AV1/src/Execution.java
@@ -1,5 +1,30 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Execution {
     public static void main(String[] args) {
+        ArrayList<Roles> role_collection = new ArrayList();
+        ArrayList employee_collection;
+
+        System.out.println("0 = Cancelar");
+        System.out.println("1 = Cadastrar cargos");
+        System.out.println("2 = Excluir cargos");
+        System.out.println("3 = Listar cargos");
+        System.out.println("4 = Editar cargos");
+        int choice = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção do menu"));
+        while(choice != 0){
+            if(choice == 1){
+                JOptionPane.showMessageDialog(null, "CADASTRANDO FUNCIONÁRIOS");
+                String input_name = (JOptionPane.showInputDialog("Digite o nome do funcionário"));
+                Float input_salary = Float.parseFloat((JOptionPane.showInputDialog("Digite o salário do funcionário")));
+                int input_id = Integer.parseInt(JOptionPane.showInputDialog("Digite o ID do funcionário"));
+
+                Roles r = new Roles(input_name, input_salary, input_id);
+                role_collection.add(r);
+            }
+        }
 
     }
+
+
 }
--- AV1/src/Sector.java
+++ AV1/src/Sector.java
@@ -1,7 +1,41 @@
 import java.util.ArrayList;
 
 public class Sector {
+
     private ArrayList employee_collection;
     private String name;
     private int employee_amount;
+
+    public Sector(ArrayList employee_collection, String name, int employee_amount) {
+        this.employee_collection = employee_collection;
+        this.name = name;
+        this.employee_amount = employee_amount;
+    }
+
+    public ArrayList getEmployee_collection() {
+        return employee_collection;
+    }
+
+    public void setEmployee_collection(ArrayList employee_collection) {
+        this.employee_collection = employee_collection;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployee_amount() {
+        return employee_amount;
+    }
+
+    public void setEmployee_amount(int employee_amount) {
+        this.employee_amount = employee_amount;
+    }
+
+
+
 }
